7224c609a6aae654db1082db98109d92a52fe19d
Pavel Vondruška <pvon@essencebs.com>
2021-10-02 13:23:02 +0200

    First Working React

---
+++ foodosk/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+var myElement = React.createElement('h1', {className:'greeting'}, 'Ahoj světe, div se!');
+ReactDOM.render(myElement,document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
