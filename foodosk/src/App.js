d2802907eb4adbbd386bcd6c8170c528c0239299
Pavel Vondruška <pvon@essencebs.com>
2021-10-03 10:36:33 +0200

    2021-10-02 backup

--- foodosk/src/App.js
+++ foodosk/src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
+import buttlogo from './media/motyli.gif';
 import './App.css';
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <p>My nested Pirated P</p>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stránka se změnami vlastností objetků</h1>
+      <p class="mainPage"><a href="https://akela.mendelu.cz/~rybicka/cviceni/js/cvic3/zadani.html" target="_blank">zadání</a></p>
+      <p class="mainPage">Najeď na motýla, kterého chceš vidět.</p>
+
+          <table border="7" align="center">
+              <th colspan="2">Naši motýli</th>
+              <tr>
+                  <td onmousedown="document.getElementById('mySelImg').innerHTML='<img src=\'./media/otakarek.gif\'>'"
+                      onmouseover="this.style.backgroundColor='yellow'"
+                      onmouseleave="this.style.backgroundColor=''">Otakárek</td>
+                  <td id="mySelImg" rowspan="4"><img src={buttlogo}></img></td>       
+              </tr>
+              <tr><td onmousedown="document.getElementById('mySelImg').innerHTML='<img src=\'./media/zlutasek.gif\'>'"
+                      onmouseover="this.style.backgroundColor='yellow'"
+                      onmouseleave="this.style.backgroundColor=''">Žluťásek</td></tr>
+              <tr><td onmousedown="document.getElementById('mySelImg').innerHTML='<img src=\'./media/babocka.gif\'>'"
+                      onmouseover="this.style.backgroundColor='yellow'"
+                      onmouseleave="this.style.backgroundColor=''">Babočka</td></tr>    
+          </table>
     </div>
   );
 }
--- foodosk/src/index.js
+++ foodosk/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
 var myElement = React.createElement('h1', {className:'greeting'}, 'Ahoj světe, div se!');
-ReactDOM.render(myElement,document.getElementById('root'));
+// ReactDOM.render(myElement,document.getElementById('root'));
 
+reactDom.render(<App/>,
+    document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
